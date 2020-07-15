045b6f09d8d3c137596413fe7ce862040324a2e6
Callum Macrae <callum@macr.ae>
2020-07-15 16:05:05 +0100

    Add shuffle method to random utils

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -30,6 +30,22 @@ export function pick(ary: any[]) {
   return ary[floorRange(0, ary.length)];
 }
 
+export function shuffle(ary: any[], inPlace = true) {
+  if (!inPlace) {
+    ary = ary.slice();
+  }
+
+  for (let i = ary.length - 1; i > 0; i--) {
+    const swap = floorRange(0, i + 1);
+    // @TODO apparently destructuring this causes performance problems?
+    const tmp = ary[i];
+    ary[i] = ary[swap];
+    ary[swap] = tmp;
+  }
+
+  return ary;
+}
+
 // Lazy guassian approximation
 export function irwinHall(n = 12) {
   let total = 0;
