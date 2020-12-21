6cc823a0a619f0d04e13f571415d7f681ee6e77a
Callum Macrae <callum@macr.ae>
2020-12-21 20:20:13 +0000

    Fill connected components

--- src/views/ConnectedComponents.vue
+++ src/views/ConnectedComponents.vue
@@ -17,7 +17,10 @@ export default {
   data: () => ({
     status: 'playing',
     width: undefined,
-    height: undefined
+    height: undefined,
+    options: {
+      antialias: false
+    }
   }),
   mounted() {
     this.setSize();
@@ -72,7 +75,7 @@ export default {
       ctx.clearRect(0, 0, width, height);
 
       ctx.strokeStyle = 'black';
-      ctx.lineWidth = 10;
+      ctx.lineWidth = 1;
 
       for (const line of this.lines) {
         ctx.beginPath();
@@ -89,12 +92,123 @@ export default {
         }
 
         ctx.stroke();
+      }
 
-        const imageData = ctx.getImageData(0, 0, width, height).data;
-        if (Math.random() < 0.01) {
-          console.log(imageData);
+      let linesOnly;
+      if (this.options.antialias) {
+        linesOnly = document.createElement('canvas');
+        linesOnly.width = ctx.canvas.width;
+        linesOnly.height = ctx.canvas.height;
+        linesOnly.getContext('2d').drawImage(ctx.canvas, 0, 0);
+      }
+
+      const imageData = ctx.getImageData(0, 0, width, height);
+
+      // -1: not hole
+      // 0: unlabelled
+      // 1+: labels
+      // maximum value is 32,767 - @todo check this is ok
+      const data = new Int16Array(width * height);
+
+      for (let i = 0; i < width * height; i++) {
+        data[i] = imageData.data[i * 4 + 3] < 100 ? 0 : -1;
+      }
+
+      let nextLabel = 1;
+      const equivalent = new Map();
+
+      for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+          const i = y * width + x;
+
+          if (data[i] === -1) {
+            continue;
+          }
+
+          const leftLabel = x ? data[i - 1] : -1;
+          const aboveLabel = y ? data[i - width] : -1;
+
+          if (leftLabel === -1 && aboveLabel === -1) {
+            data[i] = nextLabel;
+            nextLabel++;
+          } else if (leftLabel === -1) {
+            data[i] = aboveLabel;
+          } else if (aboveLabel === -1) {
+            data[i] = leftLabel;
+          } else {
+            const label = Math.min(aboveLabel, leftLabel);
+            data[i] = label;
+
+            if (aboveLabel !== leftLabel) {
+              equivalent.set(
+                Math.max(aboveLabel, leftLabel),
+                equivalent.get(label) || label
+              );
+            }
+          }
         }
       }
+
+      for (let i = 0; i < width * height; i++) {
+        if (equivalent.has(data[i])) {
+          data[i] = equivalent.get(data[i]);
+        }
+      }
+
+      const groupSizes = {};
+      for (let i = 0; i < width * height; i++) {
+        if (!groupSizes[data[i]]) {
+          groupSizes[data[i]] = 0;
+        }
+
+        groupSizes[data[i]]++;
+      }
+
+      const groupSizesFlat = Object.entries(groupSizes)
+        .filter(([label]) => label !== -1)
+        .map(([key, value]) => value);
+
+      const maxGroupSize = Math.max(...groupSizesFlat);
+
+      const groupColors = {
+        '-1': [0, 0, 0, this.options.antialias ? 0 : 255]
+      };
+      for (let [label, value] of Object.entries(groupSizes)) {
+        if (label === '-1') {
+          continue;
+        }
+
+        const color = [255, 0, 0];
+        color.push((255 / maxGroupSize) * value);
+
+        groupColors[label] = color;
+      }
+
+      const newImageData = new Uint8ClampedArray(width * height * 4);
+
+      for (let i = 0; i < data.length; i++) {
+        const label = data[i];
+
+        const color = groupColors[label];
+
+        newImageData[i * 4] = color[0];
+        newImageData[i * 4 + 1] = color[1];
+        newImageData[i * 4 + 2] = color[2];
+        newImageData[i * 4 + 3] = color[3];
+      }
+
+      const newImage = new ImageData(newImageData, width, height);
+      ctx.putImageData(newImage, 0, 0);
+
+      if (this.options.antialias) {
+        ctx.drawImage(linesOnly, 0, 0);
+      }
+
+      // convert imageData to holeData
+      // first pass CCL
+      // second pass CCL
+      // calculate what to draw
+      // putImageData
     }
   },
   computed: {
