5d5d4f4a89a4a8edcd6c4a0be309e436a684b2df
Callum Macrae <callum@macr.ae>
2021-01-21 13:21:16 +0000

    Add dat.gui

--- src/views/WarpedGrid.vue
+++ src/views/WarpedGrid.vue
@@ -12,6 +12,7 @@
 import * as lines from '../utils/lines';
 import recordMixin from '../mixins/record';
 import SimplexNoise from 'simplex-noise';
+import * as dat from 'dat.gui';
 
 const simplexX = new SimplexNoise('a');
 const simplexY = new SimplexNoise('b');
@@ -21,22 +22,50 @@ export default {
   data: () => ({
     status: 'playing',
     width: undefined,
-    height: undefined
+    height: undefined,
+    options: {
+      resolution: 2,
+      cellWidth: 100,
+      cellHeight: 140,
+      noiseXInFactor: 1 / 400,
+      noiseYInFactor: 1 / 400,
+      noiseTInFactor: 1 / 4000,
+      noiseTInOffset: 0.1,
+      noiseXOutFactor: 50,
+      noiseYOutFactor: 50
+    }
   }),
   mounted() {
     this.setSize();
     this.init();
     this.frame();
+
+    const gui = new dat.GUI();
+    this.gui = gui;
+
+    gui.add(this.options, 'resolution', 1, 3, 1);
+    gui.add(this.options, 'cellWidth', 30, 200);
+    gui.add(this.options, 'cellHeight', 30, 200);
+    gui.add(this.options, 'noiseXInFactor', 0, 0.01);
+    gui.add(this.options, 'noiseYInFactor', 0, 0.01);
+    gui.add(this.options, 'noiseTInFactor', 0, 0.003);
+    gui.add(this.options, 'noiseTInOffset', 0.01, 1);
+    gui.add(this.options, 'noiseXOutFactor', 0, 200);
+    gui.add(this.options, 'noiseYOutFactor', 0, 200);
   },
   beforeDestroy() {
     cancelAnimationFrame(this.frameId);
+
+    if (this.gui) {
+      this.gui.destroy();
+    }
   },
   methods: {
     setSize() {
       const canvas = this.$refs.canvas;
       this.ctx = canvas.getContext('2d');
 
-      const dpr = 1; // window.devicePixelRatio;
+      const dpr = window.devicePixelRatio;
       this.width = canvas.clientWidth * dpr;
       this.height = canvas.clientHeight * dpr;
       canvas.width = this.width;
@@ -51,43 +80,47 @@ export default {
       }
 
       const { ctx, width, height } = this;
+      const { noiseTInFactor } = this.options;
 
       ctx.clearRect(0, 0, width, height);
 
-      ctx.globalCompositeOperation = 'multiply'
+      ctx.globalCompositeOperation = 'multiply';
 
       ctx.fillStyle = 'red';
-      this.drawGrid(timestamp / 2e3);
+      this.drawGrid(timestamp * noiseTInFactor);
 
       ctx.fillStyle = 'blue';
-      this.drawGrid(timestamp / 2e3 + 0.1);
+      this.drawGrid(timestamp * noiseTInFactor + this.options.noiseTInOffset);
     },
     drawGrid(t) {
       const { ctx, width, height } = this;
 
-      const cellWidth = 50;
-      const cellHeight = 70;
-
-      const xFactor = 400;
-      const yFactor = 400;
-
-      const xNoiseFactor = 10;
-      const yNoiseFactor = 10;
+      const {
+        resolution,
+        cellWidth,
+        cellHeight,
+        noiseXInFactor,
+        noiseYInFactor,
+        noiseXOutFactor,
+        noiseYOutFactor
+      } = this.options;
 
       const cellsX = Math.ceil(width / cellWidth);
       const cellsY = Math.ceil(height / cellHeight);
 
       const warp = ([x, y]) => {
-        const xFactor = 400;
-        const yFactor = 400;
-        const noiseX = simplexX.noise3D(x / xFactor, y / yFactor, t) * 10;
-        const noiseY = simplexY.noise3D(x / xFactor, y / yFactor, t) * 10;
+        const noiseX =
+          simplexX.noise3D(x * noiseXInFactor, y * noiseYInFactor, t) *
+          noiseXOutFactor;
+        const noiseY =
+          simplexY.noise3D(x * noiseXInFactor, y * noiseYInFactor, t) *
+          noiseYOutFactor;
 
         return [x + noiseX, y + noiseY];
       };
 
       const lineBetween = (a, b) => {
-        const divisions = 5;
+        const divisions = { 1: 3, 2: 5, 3: 8 }[resolution];
         const offset = [(b[0] - a[0]) / divisions, (b[1] - a[1]) / divisions];
 
         for (let i = 0; i <= divisions; i++) {
