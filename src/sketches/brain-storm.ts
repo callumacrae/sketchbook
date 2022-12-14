445bb92a467994e46309066adf29a80c204e1d53
Callum Macrae <callum@macr.ae>
2022-12-14 15:14:23 +0000

    Add surrounding letters

--- src/sketches/brain-storm.ts
+++ src/sketches/brain-storm.ts
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
+import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
+import * as random from '@/utils/random';
+import * as math from '@/utils/maths';
 
 // https://www.shutterstock.com/image-illustration/man-silhouette-floating-over-colored-space-1871484967
 import figurePoints from './brain-storm-path.json';
@@ -15,6 +19,7 @@ interface CanvasState {
   scene: THREE.Scene;
   camera: THREE.PerspectiveCamera;
   outlineMaterial: MeshLineMaterial;
+  characterGroup: THREE.Group;
 }
 
 const sketchConfig = {
@@ -80,7 +85,77 @@ function initFigure({ config, width, height }: InitProps<SketchConfig>) {
   return { group: figureGroup, outlineMaterial };
 }
 
-const init: InitFn<CanvasState, SketchConfig> = (props) => {
+async function initCharacters() {
+  const characterGroup = new THREE.Group();
+
+  const loader = new FontLoader();
+  const font = await new Promise<Font>((resolve) => {
+    loader.load('/brain-storm/helvetiker_regular.typeface.json', (font) =>
+      resolve(font)
+    );
+  });
+
+  const characterRadius = 200;
+
+  const characters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const characterGeometries = characters.map((character) => {
+    const size = 10;
+    const geometry = new TextGeometry(character, {
+      font,
+      size,
+      height: 1,
+      curveSegments: 12,
+      bevelEnabled: false,
+    });
+
+    geometry.computeBoundingBox();
+    const { boundingBox } = geometry;
+    if (!boundingBox) throw new Error('??');
+    const centerOffsetX = -0.5 * (boundingBox.max.x - boundingBox.min.x);
+    // const centerOffsetY = -0.5 * (boundingBox.max.y - boundingBox.min.y);
+    const centerOffsetY = size / -2;
+
+    geometry.translate(centerOffsetX, centerOffsetY, 0);
+    return geometry;
+  });
+
+  const materials = [
+    new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), // front
+    new THREE.MeshPhongMaterial({ color: 0xffffff }), // side
+  ];
+
+  const bands = 27;
+  for (let xIndex = 0; xIndex < bands; xIndex++) {
+    const bandGroup = new THREE.Group();
+
+    const x = math.scale([-1, bands], [Math.PI / -2, Math.PI / 2], xIndex);
+    const bandRadius = characterRadius * Math.cos(x);
+    const bandCircumference = 2 * Math.PI * bandRadius;
+
+    const letterSpacing = 15;
+
+    const lettersOnBand = Math.floor(bandCircumference / letterSpacing);
+    for (let yIndex = 0; yIndex < lettersOnBand; yIndex++) {
+      const y = math.scale([0, lettersOnBand], [0, Math.PI * 2], yIndex);
+
+      const textMesh = new THREE.Mesh(
+        random.pick(characterGeometries),
+        materials
+      );
+
+      textMesh.rotateY(y);
+      textMesh.rotateX(x);
+      textMesh.translateZ(-characterRadius);
+      bandGroup.add(textMesh);
+    }
+
+    characterGroup.add(bandGroup);
+  }
+
+  return { group: characterGroup };
+}
+
+const init: InitFn<CanvasState, SketchConfig> = async (props) => {
   props.initControls(({ pane, config }) => {
     pane.addInput(config, 'lineWidth', { min: 0, max: 5 });
   });
@@ -93,10 +168,18 @@ const init: InitFn<CanvasState, SketchConfig> = (props) => {
   const lighting = initLighting();
   scene.add(lighting.group);
 
+  const characters = await initCharacters();
+  scene.add(characters.group);
+
   const figure = initFigure(props);
   scene.add(figure.group);
 
-  return { scene, camera, outlineMaterial: figure.outlineMaterial };
+  return {
+    scene,
+    camera,
+    outlineMaterial: figure.outlineMaterial,
+    characterGroup: characters.group,
+  };
 };
 
 const frame: FrameFn<CanvasState, SketchConfig> = ({
@@ -106,9 +189,12 @@ const frame: FrameFn<CanvasState, SketchConfig> = ({
 }) => {
   if (!renderer || !config) throw new Error('???');
 
-  const { scene, camera, outlineMaterial } = state;
+  const { scene, camera, outlineMaterial, characterGroup } = state;
 
   outlineMaterial.lineWidth = config.lineWidth;
+
+  const bandGroup = characterGroup.children[11];
+  bandGroup.rotateY(0.002);
 
   renderer.render(scene, camera);
 };
