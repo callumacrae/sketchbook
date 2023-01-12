1421b5d1d1bebec6aed6db8bec107726e6f15174
Callum Macrae <callum@macr.ae>
2023-01-12 20:04:24 +0000

    Make one of the mirrors reflect light 😅

--- src/sketches/moving-mirrors.ts
+++ src/sketches/moving-mirrors.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import SimplexNoise from 'simplex-noise';
 
+import { extendMaterial } from '@/utils/three-extend-material';
 import { toCanvasComponent } from '@/utils/renderers/vue';
 import type {
   Config,
@@ -9,6 +10,8 @@ import type {
   InitProps,
   FrameFn,
 } from '@/utils/renderers/vanilla';
+
+const glsl = String.raw;
 
 interface CanvasState {
   simplex: SimplexNoise;
@@ -42,17 +45,137 @@ function initCamera(
 }
 
 function initLighting(scene: THREE.Scene) {
-  const directionalLight = new THREE.PointLight(0xffffff, 0.6);
-  directionalLight.position.set(0, 0, 17);
-  scene.add(directionalLight);
+  const pointLight = new THREE.PointLight(0xffffff, 0.6);
+  pointLight.position.set(0, 0, 17);
+  scene.add(pointLight);
 
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
   scene.add(ambientLight);
 }
 
+const floorMaterial = extendMaterial(THREE.MeshPhongMaterial, {
+  class: THREE.ShaderMaterial,
+
+  header: glsl`
+    #define USE_ENVMAP
+    #define ENV_WORLDPOS
+
+    varying vec3 vMvPointLightPosition[NUM_POINT_LIGHTS];
+  `,
+
+  vertexHeader: glsl`
+    // TODO: any way of not having to copy this manually from lights_pars_begin?
+    struct PointLight {
+      vec3 position;
+      vec3 color;
+      float distance;
+      float decay;
+    };
+
+    uniform PointLight pointLights[ NUM_POINT_LIGHTS ];
+  `,
+
+  vertex: {
+    transformEnd: glsl`
+      #pragma unroll_loop_start
+      for (int i = 0; i < NUM_POINT_LIGHTS; i++) {
+        vMvPointLightPosition[i] = (projectionMatrix * modelViewMatrix * vec4(pointLights[i].position, 1.0)).xyz;
+      }
+      #pragma unroll_loop_end
+    `,
+  },
+
+  fragmentHeader: glsl`
+    uniform vec3 uMirrorAngle;
+
+    mat4 translateMatrix(vec3 v) {
+      return mat4(
+        1.0, 0.0, 0.0, v.x,
+        0.0, 1.0, 0.0, v.y,
+        0.0, 0.0, 1.0, v.z,
+        0.0, 0.0, 0.0, 1.0
+      );
+    }
+
+    mat4 rotationXMatrix(float angle) {
+      return mat4(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, cos(angle), -sin(angle), 0.0,
+        0.0, sin(angle), cos(angle), 0.0,
+        0.0, 0.0, 0.0, 1.0
+      );
+    }
+
+    mat4 rotationZMatrix(float angle) {
+      return mat4(
+        cos(angle), -sin(angle), 0.0, 0.0,
+        sin(angle), cos(angle), 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0, 1.0
+      );
+    }
+
+    mat4 scaleZMatrix(float scale) {
+      return mat4(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, scale, 0.0,
+        0.0, 0.0, 0.0, 1.0
+      );
+    }
+  `,
+
+  fragment: {
+    '#include <lights_fragment_begin>': glsl`
+      vec3 mirrorCenter = vec3(0.0, 0.0, 0.5);
+      float mirrorRadius = 1.0;
+      mat4 mirrorTransform = translateMatrix(-mirrorCenter)
+        * rotationZMatrix(-uMirrorAngle.z)* rotationXMatrix(-uMirrorAngle.x) ;
+      
+      PointLight pointLightReflection;
+      IncidentLight directLightReflection;
+
+      #pragma unroll_loop_start
+      for (int i = 0; i < NUM_POINT_LIGHTS; i++) {
+        pointLightReflection = pointLights[i];
+
+        vec3 lightPos = vMvPointLightPosition[i];
+
+        vec4 lightPosMirrorSpace = vec4(lightPos, 1.0) * mirrorTransform;
+        vec4 reflectedLightPos = lightPosMirrorSpace * scaleZMatrix(-1.0);
+        vec4 positionMirrorSpace = vec4(vWorldPosition, 1.0) * mirrorTransform;
+
+        if (positionMirrorSpace.z > 0.0) {
+          float lambda = positionMirrorSpace.z / (positionMirrorSpace.z - reflectedLightPos.z);
+          vec3 intersection = mix(positionMirrorSpace.xyz, reflectedLightPos.xyz, lambda);
+
+          if (sqrt(pow(intersection.x, 2.0) + pow(intersection.y, 2.0)) < mirrorRadius) {
+            float lightDistance = distance(reflectedLightPos.xyz, positionMirrorSpace.xyz);
+            directLightReflection.color = pointLightReflection.color;
+            directLightReflection.color *= getDistanceAttenuation(lightDistance, 20.0, 1.8);
+            reflectedLight.directSpecular += directLightReflection.color;
+          }
+        }
+      }
+      #pragma unroll_loop_end
+    `,
+  },
+
+  uniforms: {
+    diffuse: {
+      shared: true,
+      value: new THREE.Color(0xaaaaaa),
+    },
+
+    uMirrorAngle: {
+      shared: true,
+      value: new THREE.Vector3(),
+    },
+  },
+});
+
 function initFloor(scene: THREE.Scene) {
   const floorGeometry = new THREE.PlaneGeometry(100, 100);
-  const floorMaterial = new THREE.MeshPhongMaterial({ color: 0xaaaaaa });
   const floor = new THREE.Mesh(floorGeometry, floorMaterial);
   floor.rotation.z = -Math.PI / 2;
   scene.add(floor);
@@ -101,7 +224,13 @@ function initMirrors(scene: THREE.Scene) {
         0.5
       );
 
-      mirror.rotation.x = Math.PI / 4;
+      mirror.rotation.x = Math.PI / 3.6;
+      mirror.rotation.z = Math.PI / 4;
+      floorMaterial.uniforms.uMirrorAngle.value.set(
+        mirror.rotation.x,
+        mirror.rotation.y,
+        mirror.rotation.z
+      );
 
       mirrorsGroup.add(mirror);
     }
@@ -118,6 +247,15 @@ function initMirrors(scene: THREE.Scene) {
     mirrorsGroup.children.forEach((mirror) => {
       if (!(mirror instanceof THREE.Group)) return;
 
+      mirror.rotation.x = Math.PI / (3.2 + Math.sin(t) * 0.5);
+      mirror.rotation.z += 0.01;
+      floorMaterial.uniforms.uMirrorAngle.value.set(
+        mirror.rotation.x,
+        mirror.rotation.y,
+        mirror.rotation.z
+      );
+      return;
+
       const { x, y } = mirror.position;
 
       const angleNoise = state.simplex.noise3D(
@@ -133,6 +271,12 @@ function initMirrors(scene: THREE.Scene) {
         t * config.noiseTiltTimeInFactor + 100
       );
       mirror.rotation.x = (tiltNoise * Math.PI) / 2;
+
+      floorMaterial.uniforms.uMirrorAngle.value.set(
+        mirror.rotation.x,
+        mirror.rotation.y,
+        mirror.rotation.z
+      );
     });
   };
 
