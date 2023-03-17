96e770eccb465d46ea48c0ef9412134498d3acde
Callum Macrae <callum@macr.ae>
2023-03-17 17:08:18 +0000

    add noise machine

---
+++ src/sketches/noise-machine.ts
@@ -0,0 +1,106 @@
+import { easePolyIn, easePolyInOut } from 'd3-ease';
+
+import TweakpanePlugin from '@/utils/plugins/tweakpane';
+import NoiseMachine, {
+  BandedNoiseGenerator,
+  SimplexNoiseGenerator,
+  SineGenerator,
+} from '@/utils/noise';
+import type { SketchConfig, InitFn, FrameFn } from '@/utils/renderers/vanilla';
+
+export const meta = {
+  name: 'Noise machine',
+  date: '2023-03-17',
+  tags: ['Canvas 2D', 'Noise'],
+};
+
+export interface CanvasState {
+  noiseMachine: NoiseMachine;
+}
+
+const userConfig = {
+  var: 1,
+};
+export type UserConfig = typeof userConfig;
+
+const tweakpanePlugin = new TweakpanePlugin<CanvasState, UserConfig>();
+
+export const sketchConfig: Partial<SketchConfig<CanvasState, UserConfig>> = {
+  userConfig,
+  plugins: [tweakpanePlugin],
+};
+
+export const init: InitFn<CanvasState, UserConfig> = () => {
+  const noiseMachine = new NoiseMachine();
+
+  const generalNoiseFactor = 0.2;
+  const fastNoise = new SimplexNoiseGenerator({
+    inputFactor: 0.002,
+    range: [0, 1],
+  });
+  const slowNoise = new SimplexNoiseGenerator({
+    inputFactor: 0.0001,
+    range: [0, generalNoiseFactor],
+    factor: fastNoise,
+  });
+  noiseMachine.add(slowNoise);
+
+  const bandedNoiseGenerator = new BandedNoiseGenerator({
+    bandFreqency: new SimplexNoiseGenerator({
+      inputFactor: 0.00001,
+      range: [100, 150],
+    }),
+    bandSize: 50,
+    factor: new SimplexNoiseGenerator({
+      inputFactor: 0.001,
+      range: [0, 1],
+      factor(x) {
+        const slowNoiseOut = slowNoise.get(x);
+        return 0.2 + (generalNoiseFactor - slowNoiseOut) * 3;
+      },
+      easing: easePolyIn.exponent(5),
+    }),
+    easing: easePolyInOut.exponent(3),
+  });
+  noiseMachine.add(bandedNoiseGenerator);
+
+  return { noiseMachine };
+};
+
+export const frame: FrameFn<CanvasState, UserConfig> = ({
+  ctx,
+  state,
+  width,
+  height,
+  timestamp,
+}) => {
+  if (!ctx) throw new Error('???');
+
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.strokeStyle = '#494242';
+  ctx.lineWidth = 2;
+
+  ctx.beginPath();
+  ctx.moveTo(0, height / 2);
+  ctx.lineTo(width, height / 2);
+  ctx.stroke();
+
+  ctx.strokeStyle = '#e59c9c';
+
+  ctx.beginPath();
+  ctx.moveTo(0, height / 4);
+  ctx.lineTo(width, height / 4);
+  ctx.moveTo(0, (height * 3) / 4);
+  ctx.lineTo(width, (height * 3) / 4);
+  ctx.stroke();
+
+  ctx.strokeStyle = 'black';
+
+  ctx.beginPath();
+  for (let x = 0; x < width; x++) {
+    const y = state.noiseMachine.get(x + timestamp / 2);
+    ctx[x ? 'lineTo' : 'moveTo'](x, height / 2 + (-y * height) / 4);
+  }
+  ctx.stroke();
+};
---
+++ src/utils/noise.ts
@@ -0,0 +1,189 @@
+import SimplexNoise from 'simplex-noise';
+
+interface NoiseGenerator {
+  get(x: number): number;
+}
+
+export interface NoiseMachineOptions {
+  simplex: SimplexNoise;
+}
+
+export default class NoiseMachine implements NoiseGenerator {
+  private noises: NoiseGenerator[] = [];
+
+  add(noise: NoiseGenerator) {
+    this.noises.push(noise);
+    return this;
+  }
+
+  get(x: number) {
+    let noiseAtX = 0;
+
+    for (const noise of this.noises) {
+      noiseAtX += noise.get(x);
+    }
+
+    return noiseAtX;
+  }
+}
+
+type ResolvableValue = number | NoiseGenerator | ((x: number) => number);
+const resolveVal = (value: ResolvableValue, x: number) => {
+  if (typeof value === 'number') return value;
+  if (typeof value === 'function') return value(x);
+  return value.get(x);
+};
+
+export interface SimplexNoiseGeneratorOptions {
+  simplex?: SimplexNoise;
+  inputFactor?: ResolvableValue;
+  range?: [ResolvableValue, ResolvableValue];
+  factor?: ResolvableValue;
+  // x is a value between 0 and 1
+  easing?: (x: number) => number;
+}
+export class SimplexNoiseGenerator implements NoiseGenerator {
+  private simplex: SimplexNoise;
+  private options: Omit<Required<SimplexNoiseGeneratorOptions>, 'simplex'>;
+
+  private noiseSeed = Math.random() * 1e6;
+
+  constructor(options?: SimplexNoiseGeneratorOptions) {
+    this.simplex = options?.simplex || new SimplexNoise();
+
+    this.options = Object.assign(
+      {
+        inputFactor: 1,
+        range: [-1, 1],
+        factor: 1,
+        easing: (x: number) => x,
+      },
+      options
+    );
+  }
+
+  get(x: number) {
+    const inputFactor = resolveVal(this.options.inputFactor, x);
+
+    let value = this.simplex.noise2D(x * inputFactor, this.noiseSeed) / 2 + 0.5;
+
+    value = this.options.easing(value);
+
+    if (Array.isArray(this.options.range)) {
+      const min = resolveVal(this.options.range[0], x);
+      const max = resolveVal(this.options.range[1], x);
+      value = value * (max - min) + min;
+    } else {
+      value = value * 2 - 1;
+    }
+
+    if (this.options.factor) {
+      value *= resolveVal(this.options.factor, x);
+    }
+
+    return value;
+  }
+}
+
+export interface BandedNoiseGeneratorOptions {
+  range?: [ResolvableValue, ResolvableValue];
+  factor?: ResolvableValue;
+  // x is a value between 0 and 1
+  easing?: (x: number) => number;
+
+  bandFreqency: ResolvableValue;
+  bandSize: ResolvableValue;
+  bandSizeType?: 'absolute' | 'percentage';
+}
+export class BandedNoiseGenerator implements NoiseGenerator {
+  private options: Required<BandedNoiseGeneratorOptions>;
+
+  constructor(options?: BandedNoiseGeneratorOptions) {
+    this.options = Object.assign(
+      {
+        range: [0, 1],
+        factor: 1,
+        easing: (x: number) => x,
+        bandSizeType: 'absolute',
+      },
+      options
+    );
+  }
+
+  get(x: number) {
+    const bandFrequency = resolveVal(this.options.bandFreqency, x);
+    const bandSizeUntyped = resolveVal(this.options.bandSize, x);
+    const bandSize =
+      this.options.bandSizeType === 'percentage'
+        ? bandSizeUntyped * bandFrequency
+        : bandSizeUntyped;
+
+    const distFromBand = Math.abs(
+      x - Math.round(x / bandFrequency) * bandFrequency
+    );
+    let value = 0;
+    if (distFromBand < bandSize) {
+      value = (bandSize - distFromBand) / bandSize;
+    }
+
+    value = this.options.easing(value);
+
+    if (Array.isArray(this.options.range)) {
+      const min = resolveVal(this.options.range[0], x);
+      const max = resolveVal(this.options.range[1], x);
+      value = value * (max - min) + min;
+    } else {
+      value = value * 2 - 1;
+    }
+
+    if (this.options.factor) {
+      value *= resolveVal(this.options.factor, x);
+    }
+
+    return value;
+  }
+}
+
+export interface SineGeneratorOptions {
+  inputFactor?: ResolvableValue;
+  range?: [ResolvableValue, ResolvableValue];
+  factor?: ResolvableValue;
+  // x is a value between 0 and 1
+  easing?: (x: number) => number;
+}
+export class SineGenerator implements NoiseGenerator {
+  private options: Required<SineGeneratorOptions>;
+
+  constructor(options?: SineGeneratorOptions) {
+    this.options = Object.assign(
+      {
+        inputFactor: 1,
+        range: [-1, 1],
+        factor: 1,
+        easing: (x: number) => x,
+      },
+      options
+    );
+  }
+
+  get(x: number) {
+    const inputFactor = resolveVal(this.options.inputFactor, x);
+
+    let value = Math.sin(x * inputFactor) / 2 + 0.5;
+    value = this.options.easing(value);
+
+    if (Array.isArray(this.options.range)) {
+      const min = resolveVal(this.options.range[0], x);
+      const max = resolveVal(this.options.range[1], x);
+      value = value * (max - min) + min;
+    } else {
+      value = value * 2 - 1;
+    }
+
+    if (this.options.factor) {
+      value *= resolveVal(this.options.factor, x);
+    }
+
+    return value;
+  }
+}
