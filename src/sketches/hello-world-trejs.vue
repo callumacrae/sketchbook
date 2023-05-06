7a02a8e31b731e60d936927e4e1c280f0a1d983e
Callum Macrae <callum@macr.ae>
2023-05-06 16:53:49 +0100

    tresjs hello world

---
+++ src/sketches/hello-world-trejs.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import { shallowRef } from 'vue';
+import { TresCanvas, useRenderLoop } from '@tresjs/core';
+import { OrbitControls } from '@tresjs/cientos';
+import type { TresInstance } from '@tresjs/core';
+
+const meshRef = shallowRef<TresInstance>(null);
+
+const { onLoop } = useRenderLoop();
+onLoop(({ delta }) => {
+  if (!meshRef.value) return;
+  meshRef.value.rotation.x += delta;
+  meshRef.value.rotation.y += delta * 0.5;
+});
+</script>
+
+<template>
+  <TresCanvas window-size>
+    <TresPerspectiveCamera :position="[0, 0, 5]" />
+    <OrbitControls />
+    <TresMesh ref="meshRef">
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshPhongMaterial color="orange" />
+    </TresMesh>
+    <TresDirectionalLight :intensity="0.5" :position="[0, 1, 5]" />
+    <TresAmbientLight :intensity="0.5" />
+  </TresCanvas>
+</template>
+
+<script lang="ts">
+export const meta = {
+  name: 'Hello world (TresJS)',
+  date: '2023-05-06',
+  tags: ['Three.js', 'TresJS', 'Hello World'],
+};
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,14 @@ import sketchbookPlugin from './vite/sketch-plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) =>
+            tag.startsWith('Tres') && tag !== 'TresCanvas',
+        },
+      },
+    }),
     glsl({
       exclude: 'src/sketches/*.glsl',
     }),
